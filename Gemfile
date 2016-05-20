ccd029063a738f8aebc880124f3565795586b197
afei1231 <afei1231@gmail.com>
2016-05-20 17:08:57 +0800

    finished auth and post

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://ruby.taobao.org'
 
 
+gem 'puma'
+gem 'nokogiri'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,16 +1,32 @@
+require 'digest/sha1'
+require 'net/http'
+require 'nokogiri'
+
 class WechatsController < ApplicationController
-	skip_before_action :verify_authenticity_token
-	before_action :check_signature?
+	before_action :check_signature?, only: [:wechat_auth, :wechat_post]
 
   def wechat_auth
+		render text: params[:echostr]
   end
 
   def wechat_post
+		content = request.body.read.force_encoding("UTF-8")
+		Rails.logger.info( content )
+		doc = Nokogiri::Slop( content )
+    if doc.xml.MsgType.content == "event" and doc.xml.Event.content == "subscribe"
+      render "wechats/subscribe", layout: false, :formats => :xml
+    else
+      render "wechats/message", layout: false, :formats => :xml
+    end
   end
 	
+  def wechat_create_menu
+	end
+
 	private
 	
 	def check_signature?
+    Digest::SHA1.hexdigest( [params[:timestamp], params[:nonce], @@token].sort.join ) == params[:signature]
 	end
 	
 end
