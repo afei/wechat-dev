84f01048945e116961b30fe216ed81d7a7652cc4
afei1231 <afei1231@gmail.com>
2016-05-27 23:15:19 +0800

    config file

--- config/puma.rb
+++ config/puma.rb
@@ -12,11 +12,11 @@ application_path = "/Users/ichr/Rails/wechat-dev"
 directory "#{application_path}"
 
 # puma configration
-pidfile "#{application_path}/tmp/pids/puma.pid"
-state_path "#{application_path}/tmp/sockets/puma.state"
+pidfile "#{application_path}/tmp/puma.pid"
+state_path "#{application_path}/tmp/puma.state"
 stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
-bind "unix://#{application_path}/tmp/sockets/#{app_name}.sock"
-activate_control_app "unix://#{application_path}/tmp/sockets/pumactl.sock"
+bind "unix://#{application_path}/tmp/#{app_name}.sock"
+activate_control_app "unix://#{application_path}/tmp/pumactl.sock"
 
 # run as daemonize
 daemonize true
